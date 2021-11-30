c331d530da98cf8a1d70153fc01d5618a9f1d37b
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2021-11-30 19:09:23 +0100

    fix login

--- login.py
+++ login.py
@@ -5,9 +5,9 @@ users=["luigi","gianni","maria"]
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-		if request.form['username'] in users:
-			return render_template("hallo.html",nome=request.form['username'])
-		else:
-			return "Non autorizzato"
+        if request.form['username'] in users:
+            return render_template("hallo.html",nome=request.form['username'])
+        else:
+            return "Non autorizzato"
     else:
-		return render_template("login.html")
+        return render_template("login.html")
